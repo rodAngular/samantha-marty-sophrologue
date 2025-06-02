0584edeaf8861ad0b01262ff1512cff27b78102c
rodAngular <rod_didier@hotmail.fr>
2025-06-02 15:15:33 +0200

    ajout des images et différents styles sur home

--- src/app/pages/home/sections/section-two/section-two.component.ts
+++ src/app/pages/home/sections/section-two/section-two.component.ts
@@ -11,25 +11,25 @@ import { CommonModule } from '@angular/common';
 export class SectionTwoComponent {
   cards = [
     {
-      image: 'assets/images/card1.jpg',
+      image: '../../../../assets/images/Triste-sur-le-canapé.jpg',
       title: 'Stress et Anxiété',
       text: 'La sophrologie aide à gérer les troubles liés au stress et à l’anxiété. Elle offre des outils concrets pour retrouver calme et sérénité.',
       expanded: false
     },
     {
-      image: 'assets/images/card2.jpg',
+      image: '../../../../assets/images/Image-de-médicament-addiction.jpg',
       title: 'Addictions',
       text: 'Grâce à la sophrologie, il est possible de renforcer la motivation et de développer une meilleure maîtrise de soi pour sortir des comportements addictifs.',
       expanded: false
     },
     {
-      image: 'assets/images/card3.jpg',
+      image: '../../../../assets/images/Alimentation-saine.jpg',
       title: 'Troubles du Comportement Alimentaire',
       text: 'Un accompagnement sophrologique permet de restaurer l’estime de soi et d’apaiser le rapport au corps et à l’alimentation.',
       expanded: false
     },
     {
-      image: 'assets/images/card4.jpg',
+      image: '../../../../assets/images/Femme-dans-la-douleur.jpg',
       title: 'Confiance en soi',
       text: 'Travailler sur sa respiration et ses ressentis permet de renforcer la confiance en soi, étape par étape.',
       expanded: false
